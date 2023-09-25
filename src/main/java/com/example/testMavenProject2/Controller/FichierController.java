5cbb923498eb11e7df3d8166e091b094385d0237
nathanSIO <janvnathan1@gmail.com>
2023-09-25 16:48:59 +0200

    change minio version and remove all unsed import

--- src/main/java/com/example/testMavenProject2/Controller/FichierController.java
+++ src/main/java/com/example/testMavenProject2/Controller/FichierController.java
@@ -1,20 +1,13 @@
 package com.example.testMavenProject2.Controller;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.testMavenProject2.Service.UploadService;
--- src/main/java/com/example/testMavenProject2/Service/UploadService.java
+++ src/main/java/com/example/testMavenProject2/Service/UploadService.java
@@ -4,8 +4,6 @@ import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
