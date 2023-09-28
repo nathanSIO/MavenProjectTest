6fc52dcdbdb64ee3a12648160b310f099b9960b1
nathanSIO <janvnathan1@gmail.com>
2023-09-28 11:25:12 +0200

    test resolve "io.minio.errors" 1

--- src/main/java/com/example/testMavenProject2/TestMavenProject2Application.java
+++ src/main/java/com/example/testMavenProject2/TestMavenProject2Application.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
+import io.minio.errors.MinioException;
 
 
 @SpringBootApplication
 @ImportResource({"classpath*:application-context.xml"})
 public class TestMavenProject2Application extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MinioException{
 		SpringApplication.run(TestMavenProject2Application.class, args);
 	}
 }
